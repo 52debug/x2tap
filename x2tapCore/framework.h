2f601b9c940af861fa04c14ae783846c884a063a
Holli_Freed <x2360415889@outlook.com>
2019-05-21 15:23:15 +0800

    Merge x2tapCore

---
+++ x2tapCore/framework.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <iostream>
+
+#include <WinSock2.h>
+#include <netioapi.h>
+#include <iphlpapi.h>
+
+#pragma comment(lib, "ws2_32.lib")
+#pragma comment(lib, "iphlpapi.lib")
+
+using namespace std;
+
+#define STDCALL __stdcall
