e3f638d7c627dcc12a17233603538dae7702844a
Holli_Freed <x2360415889@outlook.com>
2019-05-22 21:01:21 +0800

    Fix #13 #17 #21 #22

--- x2tap/Objects/Subscribe.cs
+++ x2tap/Objects/Subscribe.cs
@@ -5,9 +5,14 @@ namespace x2tap.Objects
 	public class Subscribe
 	{
 		/// <summary>
+		///		组 ID
+		/// </summary>
+		public string GroupID = Guid.NewGuid().ToString();
+
+		/// <summary>
 		///		组名
 		/// </summary>
-		public string GroupName = Guid.NewGuid().ToString();
+		public string GroupName = String.Empty;
 
 		/// <summary>
 		///		链接
